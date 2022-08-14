02435220c5f3b7ef92a3bd12556e70f93e881fdd
Romashka <rroma13579@gmail.com>
2022-08-14 19:28:46 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var blocks = document.getElementsByClassName('blocks')[0]
 var hexagon = document.getElementById("hexagon")
 var around_mouse = document.getElementById("around-mouse")
+var lightbulb = document.getElementById("lightbulb")
 for(var k=1; k<73; k++){
     if(k==10 || k==31 || k==52){
         blocks.innerHTML += "<div id='hexagon-2'></div>"
@@ -15,8 +16,10 @@ for(var k=1; k<73; k++){
 function Mouse_Position(event){
     var x = event.clientX;
     var y = event.clientY;
-    console.log(x, y)
     around_mouse.style.display = "block"
-    around_mouse.style.top = +(y-100)+"px"
-    around_mouse.style.left = +(x-100)+"px"
+    around_mouse.style.top = +(y-215)+"px"
+    around_mouse.style.left = +(x-150)+"px"
+    lightbulb.style.display = "block"
+    lightbulb.style.top = +(y-100)+"px"
+    lightbulb.style.left = +(x-50)+"px"
 }
